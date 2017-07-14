372ffc9ae96185cb71fbc6e2aa687cda3190747f
diegomanjarres <da.manjarres1094@uniandes.edu.co>
2017-07-14 13:09:00 -0500

    implement own mongoose connection

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const Items = require('./business/items-logic.js')
 const ItemsRoutes = require('./routes/items.js')
 const TransactionsRoutes = require('./routes/transactions.js')
 //const InventoryRoutes = require('./routes/inventory.js')
+const mongoose = require('mongoose')
 
+const connect = mongoose.connect.bind(mongoose)
 module.exports = {
+  connect,
   Transactions,
   Items,
   ItemsRoutes,
