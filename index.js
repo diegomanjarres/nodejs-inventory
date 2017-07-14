4c40f54500871cf8bd6286bbbfc9e05b289189a9
diegomanjarres <da.manjarres1094@uniandes.edu.co>
2017-07-14 12:31:00 -0500

    remove inventory route
    inventory routes not ready

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Transactions = require('./business/transactions-logic')
 const Items = require('./business/cost-logic.js')
 const ItemsRoutes = require('./routes/items.js')
 const TransactionsRoutes = require('./routes/transactions.js')
-const InventoryRoutes = require('./routes/inventory.js')
+//const InventoryRoutes = require('./routes/inventory.js')
 module.exports = {
   Transactions,
   Items,
   ItemsRoutes,
   TransactionsRoutes,
-  InventoryRoutes,
+  //InventoryRoutes,
 
 }
